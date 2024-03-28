937a37987d6d624d34307800e239d6aebf763e14
Sanya Korovkina <sasha@cmi2i.com>
2024-03-28 11:38:26 +0000

    up to password entry

--- main.py
+++ main.py
@@ -10,14 +10,21 @@ driver = webdriver.Chrome()
 url = "https://www.mcdstuff.co.uk/portal.php?site=login&page=mcdonaldsUKLogin"
 driver.get(url)
 
+# login button click
 button = WebDriverWait(driver, 30).until(
     EC.element_to_be_clickable((By.ID, "sso-login-button"))
 )
-
 button.click()
 time.sleep(5)
 
+# save language preferences
 save_button = driver.find_element(By.ID, "btnSetPopup")
 save_button.click()
+time.sleep(5)
 
+# enter the franchise manager
+div_element = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//div[@class='title' and contains(text(), 'Restaurant Managers & Franchisees')]"))
+    )
+div_element.click()
 time.sleep(5)
