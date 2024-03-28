fb48318f254ffb840f4dc5b6969a41c09e9a2b39
Rhys <48352444+Moderock@users.noreply.github.com>
2024-03-28 21:35:08 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,66 +1,171 @@
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
 import time
+import datetime
 
 driver = webdriver.Chrome()
 
-# Open the website
-url = "https://www.mcdstuff.co.uk/portal.php?site=login&page=mcdonaldsUKLogin"
-driver.get(url)
 
-# login button click
-button = WebDriverWait(driver, 30).until(
-    EC.element_to_be_clickable((By.ID, "sso-login-button"))
-)
-button.click()
-time.sleep(5)
-
-# save language preferences
-save_button = driver.find_element(By.ID, "btnSetPopup")
-save_button.click()
-time.sleep(5)
-
-# enter the franchise manager
-div_element = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, "//div[@class='title' and contains(text(), 'Restaurant Managers & Franchisees')]"))
+def select_from_dropdown(page, dropdown_xpath, option_text, wait=10, sleep=3):
+    dropdown = WebDriverWait(page, wait).until(
+        EC.element_to_be_clickable((By.XPATH, dropdown_xpath))
     )
-div_element.click()
-time.sleep(5)
+    select = Select(dropdown)
+    select.select_by_visible_text(option_text)
+    time.sleep(sleep)
 
-username_input = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.ID, "UsernameInputTxtManagers"))
-)
-username_input.click()
-username_input.clear()
-username_input.send_keys("ee949499")
 
-password_input = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.ID, "PasswordInputManagers"))
+def driver_page(url):
+
+    return driver
+
+
+def sender_id(page, button, input, wait=10, sleep=3):
+    change_button = WebDriverWait(page, wait).until(
+        EC.element_to_be_clickable((By.ID, button))
     )
+    change_button.click()
+    change_button.clear()
+    change_button.send_keys(input)
+    time.sleep(sleep)
 
-password_input.click()
-password_input.clear()
-password_input.send_keys("Cervantes_cag2016")
 
-login_button = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.ID, "btnLoginManagers"))
-)
-login_button.click()
-time.sleep(3)
-
-url = "https://www.mcdstuff.co.uk/portal.php?site=hrAdmin&page=hrAdminEmployees&wrap"
-driver.get(url)
-filter_input = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, "/html/body/div/div[2]/portal-ui/div/portal-ui/div[3]/div[1]/section[1]/input"))
+def clicker_id(page, button, wait=10, sleep=3):
+    change_button = WebDriverWait(page, wait).until(
+        EC.element_to_be_clickable((By.ID, button))
     )
-filter_input.click()
-filter_input.clear()
-filter_input.send_keys("100972")
+    change_button.click()
+    time.sleep(sleep)
 
 
+def clicker_xpath(page, button, wait=10, sleep=3):
+    change_button = WebDriverWait(page, wait).until(
+        EC.element_to_be_clickable((By.XPATH, button))
+    )
+    change_button.click()
+    time.sleep(sleep)
 
 
-time.sleep(60)
+def sender_xpath(page, button, input, wait=10, sleep=3):
+    change_button = WebDriverWait(page, wait).until(
+        EC.element_to_be_clickable((By.XPATH, button))
+    )
+    change_button.click()
+    change_button.clear()
+    change_button.send_keys(input)
+    time.sleep(sleep)
+
+
+def login(user_name, password):
+    print("Logging in...")
+    login_url  = "https://www.mcdstuff.co.uk/portal.php?site=login&page=mcdonaldsUKLogin"
+    driver.get(login_url )
+    clicker_xpath(driver, "/html/body/portal-ui/div/div[1]/a[1]/span[2]")
+    clicker_id(driver, "btnSetPopup")
+    login_id = "//div[@class='title' and contains(text(), 'Restaurant Managers & Franchisees')]"
+    clicker_xpath(driver, login_id)
+    sender_id(driver, "UsernameInputTxtManagers", user_name)
+    sender_id(driver, "PasswordInputManagers", password)
+    clicker_id(driver, "btnLoginManagers", wait=30)
+    hr_page = "/html/body/div[3]/portal-ui/div[3]/a[15]/div"
+    clicker_xpath(driver, hr_page, wait=30)
+    original_window = driver.current_window_handle
+    all_windows = driver.window_handles
+    for window in all_windows:
+        if window != original_window:
+            driver.switch_to.window(window)
+    return driver.current_window_handle
+
+
+def update_employee(employee_id, new_hourly_rate, driver_window_handle):
+    driver.switch_to.window(driver_window_handle)
+    time.sleep(10)
+    print("Filtering for employee...")
+    filter_employee = '/html/body/div/div[2]/portal-ui/div/portal-ui/div[3]/div[1]/section[1]/input'
+    sender_xpath(driver, filter_employee, employee_id, wait=30)
+
+    print("Click employee actions...")
+    click_actions = "/html/body/div/div[2]/portal-ui/div/portal-ui/div[3]/div[2]/div[1]/div/div/div/button"
+    clicker_xpath(driver, click_actions)
+
+    print("Click manage assignments...")
+    click_manage_assignments = "/html/body/div/div[2]/portal-ui/div/portal-ui/div[3]/div[2]/div[1]/div/div/div[2]/div/div[1]/a[5]"
+    clicker_xpath(driver, click_manage_assignments, wait=30)
+
+    print("Get new driver page...")
+    original_window = driver.current_window_handle
+    all_windows = driver.window_handles
+    for window in all_windows:
+        if window != original_window:
+            driver.switch_to.window(window)
+    time.sleep(10)
+
+    print("Click change...")
+    click_change = "/html/body/div[1]/div[2]/portal-ui/div/portal-ui/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div[2]/button[1]"
+    clicker_xpath(driver, click_change)
+
+    print("select change type dropdown...")
+    change_type_dropdown = "/html/body/div[3]/div/portal-ui/div/select[1]"
+    clicker_xpath(driver, change_type_dropdown)
+
+    print("Select Pay Change value...")
+    change_type = "/html/body/div[3]/div/portal-ui/div/select[1]"
+    select_from_dropdown(driver, change_type, "Pay Change")
+
+    print("select change reason dropdown...")
+    change_reason_dropdown = "/html/body/div[3]/div/portal-ui/div/select"
+    clicker_xpath(driver, change_reason_dropdown)
+
+    print("Select Pay Change reason...")
+    change_reason = "/html/body/div[3]/div/portal-ui/div/select[2]"
+    select_from_dropdown(driver, change_reason, "Annual Review")
+
+    print("Set as of date...")
+
+
+    as_of_date = "/html/body/div[3]/div/portal-ui/div/div/div/input[2]"
+    today = datetime.date.today()
+    today_string = today.strftime("%d/%m/%Y")
+    # sender_xpath(driver, as_of_date, today_string)
+    # as_of_date.send_keys(Keys.Enter)
+    # time.sleep(10)
+    change_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, as_of_date))
+    )
+    change_button.click()
+    change_button.clear()
+    change_button.send_keys(Keys.ENTER)
+    time.sleep(10)
+    
+    print("Click add...")
+    #     add_change = "/html/body/div[3]/div/portal-ui/div/button[1]"
+    # clicker_xpath(driver, add_change)
+    add_change = "/html/body/div[3]/div/portal-ui/div/button[1]"
+    add_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, add_change))
+    )
+    add_button.click()
+
+    # hourly_rate = "/html/body/div[1]/div[2]/portal-ui/div/portal-ui/div/div[2]/div/div/div[4]/div[8]/div[2]/div/div/div/div/input"
+    # sender_xpath(management_page, hourly_rate, new_hourly_rate)
+
+    # click_submit = "/html/body/div[1]/div[2]/portal-ui/div/portal-ui/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div[1]/button[4]"
+    # # clicker_xpath(management_page, click_submit)
+    #
+    # click_cancel = "/html/body/div[1]/div[2]/portal-ui/div/portal-ui/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div[1]/button[6]"
+    # clicker_xpath(management_page, click_cancel)
+    print("Close tab...")
+    driver.close()
+
+
+if __name__ == "__main__":
+    driver_handle = login(user_name="ee949499", password="Cervantes_cag2016")
+    update_employee(employee_id=109197, new_hourly_rate="102.24", driver_window_handle=driver_handle)
+    update_employee(employee_id=124441, new_hourly_rate="102.24", driver_window_handle=driver_handle)
+    update_employee(employee_id=153193, new_hourly_rate="102.24", driver_window_handle=driver_handle)
+    update_employee(employee_id=210458, new_hourly_rate="102.24", driver_window_handle=driver_handle)
+
