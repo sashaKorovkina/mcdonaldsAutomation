6602940652473f7e4a533f63f7dcbfac31748fb1
Sanya Korovkina <sasha@cmi2i.com>
2024-03-28 11:55:12 +0000

    up to password entry

--- main.py
+++ main.py
@@ -28,3 +28,18 @@ div_element = WebDriverWait(driver, 10).until(
     )
 div_element.click()
 time.sleep(5)
+
+username_input = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.ID, "UsernameInputTxtManagers"))
+)
+username_input.click()
+username_input.clear()
+username_input.send_keys("ee949499")
+
+password_input = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "PasswordInputManagers"))
+    )
+
+password_input.click()
+password_input.clear()
+password_input.send_keys("Cervantes_cag2016")
